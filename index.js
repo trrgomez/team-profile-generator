d2fbc9cc563a0209e5cc6e1706c092b278a680e9
trgomez <88842062+trrgomez@users.noreply.github.com>
2022-01-03 12:53:14 -0600

    added a function to prompt employee information

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const promptUser = () => {
       type: "input",
       name: "name",
       message: "Who is the manager of this team?",
-      validate: (nameInput) => {
-        if (nameInput) {
+      validate: (name) => {
+        if (name) {
           return true;
         } else {
           console.log("Please enter the manager's name!");
@@ -26,8 +26,8 @@ const promptUser = () => {
       type: "input",
       name: "id",
       message: "Please enter the manager's ID",
-      validate: (nameInput) => {
-        if (nameInput) {
+      validate: (id) => {
+        if (id) {
           return true;
         } else {
           console.log("Please enter the manager's ID!");
@@ -39,8 +39,8 @@ const promptUser = () => {
       type: "input",
       name: "email",
       message: "Enter the manager's email",
-      validate: (nameInput) => {
-        if (nameInput) {
+      validate: (email) => {
+        if (email) {
           return true;
         } else {
           console.log("Please enter the manager's email!");
@@ -52,8 +52,8 @@ const promptUser = () => {
       type: "input",
       name: "officeNumber",
       message: "Enter the manager's office number",
-      validate: (nameInput) => {
-        if (nameInput) {
+      validate: (officeNumber) => {
+        if (officeNumber) {
           return true;
         } else {
           console.log("Please enter an offic number!");
@@ -61,15 +61,125 @@ const promptUser = () => {
         }
       },
     },
-  ])
+  ]);
 };
 
+const promptEmployee = () => {
+  console.log(`
+    =================
+    Adding employees to the team
+    =================
+    `);
 
+  return inquirer.prompt([
+    {
+      type: "list",
+      name: "role",
+      message: "Please choose your employee's role",
+      choices: ["Engineer", "Intern"],
+    },
+    {
+      type: "input",
+      name: "name",
+      message: "What's the name of the employee?",
+      validate: (name) => {
+        if (name) {
+          return true;
+        } else {
+          console.log("Please enter an employee's name!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Enter the employee's ID",
+      validate: (id) => {
+        if (id) {
+          return true;
+        } else {
+          console.log("Please enter an employee's ID!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter the employee's email",
+      validate: (email) => {
+        if (email) {
+          return true;
+        } else {
+          console.log("Please enter an employee's email!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "Enter the employee's github username.",
+      when: (input) => input.role === "Engineer",
+      validate: (username) => {
+        if (username) {
+          return true;
+        } else {
+          console.log("Please enter the employee's github username!");
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "Enter the intern's school",
+      when: (input) => input.role === "Intern",
+      validate: (school) => {
+        if (school) {
+          return true;
+        } else {
+          console.log("Please enter the intern's school!");
+        }
+      },
+    },
+    {
+      type: "confirm",
+      name: "confirmAddEmployee",
+      message: "Would you like to add more team members?",
+      default: false,
+    },
+  ]);
+};
 
-promptUser().then(answers => {
-    const {name, id, email, officeNumber} = answers;
-    const manager = new Manager(name, id, email, officeNumber)
+promptUser()
+.then((answers) => {
+  const { name, id, email, officeNumber } = answers;
+  const manager = new Manager(name, id, email, officeNumber);
 
-    team.push(manager)
-    console.log(manager)
+  team.push(manager);
+  console.log(manager);
 })
+.then(promptEmployee)
+.then(answers => {
+    let {name, id, email, role, username, school, confirmAddEmployee} = answers;
+    let employee
+    
+    if(role === "Engineer"){
+        employee = new Engineer(name, id, email, username)
+        console.log(employee)
+    }else if (role === "Intern"){
+        employee = new Intern(name, id, email, school)
+        console.log(employee)
+    }else {
+        return false
+    }
+
+    team.push(employee)
+
+    if(confirmAddEmployee){
+        return promptEmployee(team)
+    }else {
+        return team
+    }
+});
